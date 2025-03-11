c44091391c15b68a3d504aa623ae5e17fe713049
Carl Justin Banate <carljustin.banate@vertexbizapp.com>
2025-03-11 17:27:39 +0800

    update

--- src/components/appointment/AppointmentForm.tsx
+++ src/components/appointment/AppointmentForm.tsx
@@ -3,14 +3,14 @@ import React, { FormEvent } from 'react';
 const AppointmentForm = () => {
   return (
     <div className="flex flex-col p-8 space-y-4">
-      <div className="solid basis-1/3 font-bold">Full Name:</div>
+      <div className="solid basis-1/3 font-bold">Service Name:</div>
       <input className="mb-8 p-2 border-1 border-solid rounded-md" />
 
-      <div className="solid basis-1/3 font-bold">Phone Number:</div>
-      <input className="mb-8 p-2 border-1 border-solid rounded-md" />
+      <div className="solid basis-1/3 font-bold">Time:</div>
+      <input type='time' className="mb-8 p-2 border-1 border-solid rounded-md" />
 
-      <div className="solid basis-1/3 font-bold">Email:</div>
-      <input className="mb-8 p-2 border-1 border-solid rounded-md" />
+      <div className="solid basis-1/3 font-bold">Date:</div>
+      <input type='date' className="mb-8 p-2 border-1 border-solid rounded-md" />
 
       <button
         type="submit"
