b8643d602359fa91afbf012aec0129328ba008c4
ZidaneOng <zidaneong777@gmail.com>
2025-03-11 14:58:28 +0800

    Update

--- src/components/section/Section.tsx
+++ src/components/section/Section.tsx
@@ -5,17 +5,19 @@ import BookingForm from '../booking/BookingForm';
 import Information from '../information/Information';
 import AppointmentForm from '../appointment/AppointmentForm';
 import NavigationBar from '../nav/NavigationBar';
+import Sidebar from '../sidebar/Sidebar';
 
 export const Section = () => {
   return (
-    <div className="flex flex-col gap-10"> {/* Ensure flex for gap to work */}
+    <div className="flex flex-col gap-10"> 
       <NavigationBar />
 
-      {/* Main Layout */}
-      <div className="flex flex-row gap-10"> {/* Added gap for columns */}
-        <div className="basis-1/3 bg-gray-200 p-4 rounded-lg">Side Bar</div>
+      <div className="flex flex-row gap-10">
+        <div className="basis-1/9 bg-gray-200 p-4 rounded-lg">
+        <Sidebar />
+        </div>
 
-        <div className="basis-1/3 space-y-10"> {/* Space between Booking & Appointment Forms */}
+        <div className="basis-5/9 space-y-10"> 
           <BookingForm />
         </div>
 
