2e6fd34d268dedf42eb744e88e73dc3fa96a79ed
Carl Justin Banate <carljustin.banate@vertexbizapp.com>
2025-03-11 11:39:21 +0800

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,8 +6,8 @@ import Image from 'next/image';
 
 export default function Home() {
   return (
-    <div className="grid min-h-screen grid-rows-[20px_1fr_20px] items-center justify-items-center gap-16 p-8 pb-20 font-[family-name:var(--font-geist-sans)] sm:p-20">
-      <main className="row-start-2 flex flex-col items-center gap-8 sm:items-start">
+    <div className="grid min-h-screen grid-rows-[20px_1fr_20px] font-[family-name:var(--font-geist-sans)] sm:p-10">
+      <main className="row-start-2 flex flex-row items-center justify-center gap-8 sm:items-start">
         <Section/>
       </main>
       <footer className="row-start-3 flex flex-wrap items-center justify-center gap-6"></footer>
