2bd50a19d3352fcdffd8590b7c440adf8709d0cf
sean <smbg1015@gmail.com>
2025-03-11 16:03:30 +0800

    update

--- src/components/appointment/AppointmentForm.tsx
+++ src/components/appointment/AppointmentForm.tsx
@@ -2,17 +2,24 @@ import React from 'react';
 
 const AppointmentForm = () => {
   return (
-    <div className="flex flex-col p-8 ...">
-      <div className="solid basis-1/3">Full Name:</div>
-      <input className="mb-8 border-4 border-double"></input>
-      <div className="solid basis-1/3">Phone Number:</div>
-      <input className="mb-8 border-4 border-double"></input>
-      <div className="solid basis-1/3">Email:</div>
-      <input className="mb-8 border-4 border-double"></input>
-      <button type="submit" className="bg-green-500 ..." onClick={() => {}}>
+    <div className="flex flex-col p-8 space-y-4">
+      <div className="solid basis-1/3 font-bold">Full Name:</div>
+      <input className="mb-8 p-2 border-1 border-solid rounded-md" />
+
+      <div className="solid basis-1/3 font-bold">Phone Number:</div>
+      <input className="mb-8 p-2 border-1 border-solid rounded-md" />
+
+      <div className="solid basis-1/3 font-bold">Email:</div>
+      <input className="mb-8 p-2 border-1 border-solid rounded-md" />
+
+      <button
+        type="submit"
+        className="bg-green-500 text-white p-2 rounded-md hover:bg-green-600"
+        onClick={() => {}}
+      >
         Submit
       </button>
-    </div>
+      </div>
   );
 };
 
