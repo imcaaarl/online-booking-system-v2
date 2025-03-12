01484998583e511526fecc01a71677e0010e904c
Carl Justin Banate <carljustin.banate@vertexbizapp.com>
2025-03-12 15:45:46 +0800

    update

---
+++ src/components/information/StarIcon.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+
+interface Properties {
+  rating: string;
+}
+
+const StarIcon = (properties: Properties) => {
+  const { rating } = properties;
+
+  const generateStars = (counter: number) =>
+    new Array(counter).fill('').map((value) => {
+      return (
+        <div>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth="1.5"
+            stroke="currentColor"
+            className="size-6"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M11.48 3.499a.562.562 0 0 1 1.04 0l2.125 5.111a.563.563 0 0 0 .475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 0 0-.182.557l1.285 5.385a.562.562 0 0 1-.84.61l-4.725-2.885a.562.562 0 0 0-.586 0L6.982 20.54a.562.562 0 0 1-.84-.61l1.285-5.386a.562.562 0 0 0-.182-.557l-4.204-3.602a.562.562 0 0 1 .321-.988l5.518-.442a.563.563 0 0 0 .475-.345L11.48 3.5Z"
+            />
+          </svg>
+        </div>
+      );
+    });
+
+  return <div className='flex flex-row'>
+    {generateStars(Number(rating))}
+    </div>;
+};
+
+export default StarIcon;
