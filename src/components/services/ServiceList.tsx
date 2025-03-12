66e2dffb675cb41a826cd6b5a57c8d93908be9a0
sean <smbg1015@gmail.com>
2025-03-12 17:15:28 +0800

    update

--- src/components/services/ServiceList.tsx
+++ src/components/services/ServiceList.tsx
@@ -1,18 +1,16 @@
 'use client';
 
 import { useEffect, useState } from 'react';
-import { useForm, SubmitHandler } from "react-hook-form";
+import { useForm, SubmitHandler } from 'react-hook-form';
 import './ServiceList.css';
 import CreateUserForm from './CreateServiceForm';
 import CreateServiceForm from './CreateServiceForm';
 
 type UserType = {
   name: string;
-
-}
+};
 
 export default function UserList() {
-
   // [getter, setter] = useState(default value)
   const [sample, setSample] = useState('the quick brown fox');
 
@@ -28,44 +26,43 @@ export default function UserList() {
       const response = await fetch('/api/services');
       const allServices = await response.json();
 
-      setServices(allServices)
+      setServices(allServices);
 
       console.log('allServices: ', allServices);
 
-      console.log('services: ', services)
-    })()
-  }, [])
+      console.log('services: ', services);
+    })();
+  }, []);
 
   const handleCreateservice = () => {
     setIsModalOpen(true);
-  }
+  };
 
   const handleClose = () => {
     setIsModalOpen(false);
-  }
-  
+  };
 
   console.log('services: ', services);
   return (
     <div className="mt-12 mb-8 flex flex-col gap-12">
       <div className="table-container">
         <div className="table-header">
-          <h6 className="table-title">List of all Services - {sample}
-          <button
-            onClick={handleCreateservice}
-            className="float-right px-4 py-2 bg-blue-600 text-white rounded-md hover:bg-blue-700"
-          >
-            Create Service
-          </button>
+          <h6 className="table-title">
+            List of all Services - {sample}
+            <button
+              onClick={handleCreateservice}
+              className="float-right rounded-md bg-blue-600 px-4 py-2 text-white hover:bg-blue-700"
+            >
+              Create Service
+            </button>
           </h6>
         </div>
-        
-        
+
         <div className="table-wrapper">
           <table className="table">
             <thead>
               <tr>
-                {['Name', 'Username' , 'Action', ''].map((heading, index) => (
+                {['Name', 'Username', 'Action', ''].map((heading, index) => (
                   <th key={index} className="th">
                     <p className="th-text">{heading.toLowerCase()}</p>
                   </th>
@@ -93,7 +90,7 @@ export default function UserList() {
           </table>
         </div>
       </div>
-      <CreateServiceForm isModalOpen={isModalOpen} onClose={handleClose}/>
+      <CreateServiceForm isModalOpen={isModalOpen} onClose={handleClose} />
     </div>
   );
 }
